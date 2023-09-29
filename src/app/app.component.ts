697024c6901afb36feba576a31d5d3c37f397b31
Felice Nicolas Barcellona <felicebarcellona1@gmail.com>
2023-09-29 15:20:50 +0200

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'interactive-rating-component-main-angular';
+
+  rate: number | null = null;
+  active: number | null = null;
+  toggle: boolean = true;
+
+  setActive(val: number) {
+    this.active = val;
+  }
+
+  submit(form: NgForm) {
+    this.rate = form.value.rate
+    this.toggle = !this.toggle
+  }
 }
